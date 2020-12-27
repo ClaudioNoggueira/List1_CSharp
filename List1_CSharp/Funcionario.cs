3869725a7e4029c0d2e7f5009decf41d97f6ea41
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-27 15:52:25 -0300

    First commit

---
+++ List1_CSharp/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace List1_CSharp {
+    class Funcionario {
+        public int _id { get; set; }
+        public string _nome { get; set; }
+        public float _salario { get; set; }
+
+        public Funcionario(int id, string nome, float salario) {
+            _id = id;
+            _nome = nome;
+            _salario = salario;
+        }
+
+        public void aumentarSalario(float porcentagem) {
+            _salario += (_salario * porcentagem / 100);
+        }
+
+        public override string ToString() {
+            return _id + ", " + _nome + ", " + _salario;
+        }
+    }
+}
---
+++ List1_CSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List1_CSharp {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario funcionario;
+
+            Console.WriteLine("How many employees will be registered?");
+            int qtdeEmpregados = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            for (int i = 1; i <= qtdeEmpregados; i++) {
+                Console.WriteLine("\nEmpregado " + i);
+
+                Console.WriteLine("\nDigite o ID do funcionário.");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite o nome do funcionário");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("\nDigite o salário do funcionário");
+                float salario = float.Parse(Console.ReadLine());
+
+                funcionario = new Funcionario(id, nome, salario);
+                funcionarios.Add(funcionario);
+            }
+
+            Console.WriteLine("\n\nDigite o ID do funcionário que terá o salário aumentado");
+            int id2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite a porcentagem do aumento");
+            float porcentagem = float.Parse(Console.ReadLine());
+
+            funcionario = funcionarios.Find(x => x._id == id2);
+            funcionario.aumentarSalario(porcentagem);
+
+            Console.WriteLine("\n\n\nLista de funcionários atualizada");
+
+            foreach (Funcionario func in funcionarios) {
+                Console.WriteLine(func.ToString());
+            }
+        }
+    }
+}
